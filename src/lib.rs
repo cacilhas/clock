e4589dc38cf1a35e6a302e527424aae2aa9d5fcd
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-06 14:19:05 -0300

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! ![screenshot](https://github.com/cacilhas/clock/blob/master/screenshot.png)
+//!
+//! Yet another [X11](https://en.wikipedia.org/wiki/X_Window_System) clock.
+//!
+//! Escape key to quit.
+//!
+//! # Installation
+//!
+//! ```sh
+//! cargo install kodumaro-clock
+//! ```
+//!
+//! # License
+//!
+//! - [3-Clause BSD License](https://opensource.org/license/bsd-3-clause/)
+//! - [COPYING](https://github.com/cacilhas/clock/blob/master/COPYING)
+
 pub mod background;
 pub mod clock;
 pub mod drawable;
