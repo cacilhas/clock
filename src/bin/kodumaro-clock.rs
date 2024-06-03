45a973c319efe760d8688045185c11e36ebb060e
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-06-03 19:36:18 -0300

    set X11 properties

--- src/bin/kodumaro-clock.rs
+++ src/bin/kodumaro-clock.rs
@@ -1,9 +1,10 @@
 extern crate kodumaro_clock;
 
-use std::error;
+use std::{error, mem, ptr};
 
 use kodumaro_clock::prelude::*;
 use raylib::prelude::*;
+use x11::xlib::{self, Atom, Display, Window, XWindowAttributes};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let (w, h) = (200.0, 200.0);
@@ -21,6 +22,14 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let clock = Clock::default();
     let background = Background::new(&mut handle, &thr, centre, clock.rotation)?;
     handle.set_window_title(&thr, "Kodumaro Clock");
+
+    unsafe {
+        let display = x11::xlib::XOpenDisplay(ptr::null());
+        if !display.is_null() {
+            let window = handle.get_window_handle();
+            set_wm_window_type(display, window as _);
+        }
+    }
 
     while !handle.window_should_close() {
         if handle.is_key_released(KeyboardKey::KEY_Q) {
@@ -48,4 +57,32 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     }
 
     Ok(())
+}
+
+unsafe fn set_wm_window_type(handle: *mut Display, window: Window) {
+    let xlib = libc::dlopen("libX11.so".as_ptr() as *const i8, libc::RTLD_NOW);
+    if xlib.is_null() {
+        return;
+    }
+    let display = xlib::XOpenDisplay(ptr::null());
+    if display.is_null() {
+        return;
+    }
+    let mut attr: XWindowAttributes = mem::zeroed();
+    xlib::XGetWindowAttributes(handle, window, &mut attr);
+
+    let atom: Atom = xlib::XInternAtom(handle, "_NET_WM_STATE".as_ptr() as *const i8, 0);
+    let below: Atom = xlib::XInternAtom(handle, "_NET_WM_STATE_BELOW".as_ptr() as *const i8, 0);
+    let hidden: Atom = xlib::XInternAtom(handle, "_NET_WM_STATE_HIDDEN".as_ptr() as *const i8, 0);
+    let data = &[below, hidden] as *const _ as *mut u8;
+    xlib::XChangeProperty(
+        handle,
+        window,
+        atom,
+        Atom::from(xlib::XA_ATOM),
+        32,
+        0,
+        data,
+        1,
+    );
 }
